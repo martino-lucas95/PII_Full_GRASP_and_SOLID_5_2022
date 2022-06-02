3ab272aeecb406b9a0e03ac1305ff79824b4d315
martino-lucas95 <71530057+martino-lucas95@users.noreply.github.com>
2022-06-02 19:15:09 -0300

    DIP

--- src/Library/ConsolePrinter.cs
+++ src/Library/ConsolePrinter.cs
@@ -4,9 +4,9 @@ namespace Full_GRASP_And_SOLID
 {
     public class ConsolePrinter : IPrinter
     {
-        public void PrintRecipe(Recipe recipe)
+        public void PrintRecipe(IRecipeContent recipeContent)
         {
-            Console.WriteLine(recipe.GetTextToPrint());
+            Console.WriteLine(recipeContent.GetTextToPrint());
         }
     }
 }
--- src/Library/FilePrinter.cs
+++ src/Library/FilePrinter.cs
@@ -4,9 +4,9 @@ namespace Full_GRASP_And_SOLID
 {
     public class FilePrinter : IPrinter
     {
-        public void PrintRecipe(Recipe recipe)
+        public void PrintRecipe(IRecipeContent recipeContent)
         {
-            File.WriteAllText("Recipe.txt", recipe.GetTextToPrint());
+            File.WriteAllText("Recipe.txt", recipeContent.GetTextToPrint());
         }
     }
 }
